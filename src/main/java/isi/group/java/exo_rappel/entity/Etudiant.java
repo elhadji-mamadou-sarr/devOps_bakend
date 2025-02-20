5a4ee61ce94a2c774c9cca38197287239d46e722
elhadji-mamadou-sarr <ehm.sarr3@isepdiamniadio.edu.sn>
2025-02-20 11:12:57 +0000

    Ajout de la config

--- src/main/java/isi/group/java/exo_rappel/entity/Etudiant.java
+++ src/main/java/isi/group/java/exo_rappel/entity/Etudiant.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
 
 @Entity
+//@Document(indexName = "etudiant")
 public class Etudiant {
 
     @Id
